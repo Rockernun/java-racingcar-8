957100bf5b281ae70cfb2f9ef919533af25503be
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-10-25 19:31:02 +0900

    refactor(view): 입력값 파싱 제거
    - 자동차 이름과 라운드 수를 문자열 그대로 반환하도록 처리
    - InputView는 입력 받는 책임 하나 밖에 없음

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,11 +6,11 @@ public class InputView {
 
     public String inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        return Console.readLine().trim();
+        return Console.readLine();
     }
 
-    public int inputRounds() {
+    public String inputRounds() {
         System.out.println("시도할 횟수는 몇 회인가요?");
-        return Integer.parseInt(Console.readLine());
+        return Console.readLine();
     }
 }
