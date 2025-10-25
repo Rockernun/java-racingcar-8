2f83397e6e0593479b5e60a0857122a92a309586
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-10-25 19:31:20 +0900

    refactor(controller): 라운드 수 파싱
    - Integer.parseInt로 라운드 수 파싱을 컨트롤러에서 수행하도록 처리

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -22,7 +22,7 @@ public class GameController {
 
     public void run() {
         List<Car> cars = gameService.initCarList(inputView.inputCarNames());
-        int rounds = inputView.inputRounds();
+        int rounds = Integer.parseInt(inputView.inputRounds());
 
         System.out.println("실행 결과");
         for (int i = 0; i < rounds; i++) {
