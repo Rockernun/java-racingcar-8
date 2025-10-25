8b055c77ebe7887cdf08d39994ebbead184f8789
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-10-25 20:11:37 +0900

    refactor(controller): 시도 횟수 파싱을 서비스에 위임
    - GameService.parseRounds(): 결과로 올바른 시도 횟수 형식을 사용하도록 처리

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -22,7 +22,7 @@ public class GameController {
 
     public void run() {
         List<Car> cars = gameService.initCarList(inputView.inputCarNames());
-        int rounds = Integer.parseInt(inputView.inputRounds());
+        int rounds = gameService.parseRounds(inputView.inputRounds());
 
         System.out.println("실행 결과");
         for (int i = 0; i < rounds; i++) {
