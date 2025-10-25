8d54de969fb13ab603fe2afc53bf1dbfe164fb18
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-10-25 20:31:46 +0900

    test(service): GameService 테스트 코드 추가
    - 자동차 이름 중복 예외 테스트 추가(공백을 포함한 중복 케이스도 포함)
    - 라운드 입력 검증 테스트 추가: 숫자가 아니거나 공백, 문자와 숫자가 뒤섞인 경우와 0 혹은 음수인 경우도 추가
    - 이름의 길이와 공백 검증에 대한 검증은 CarTest로 이동

--- src/test/java/racingcar/service/GameServiceTest.java
+++ src/test/java/racingcar/service/GameServiceTest.java
@@ -1,27 +1,52 @@
 package racingcar.service;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
 class GameServiceTest {
 
     @Test
-    void 이름_5자_초과() {
+    void 자동차_이름_중복() {
         assertSimpleTest(() -> {
-            GameService gameService = new GameService(() -> true);
-            assertThrows(IllegalArgumentException.class, () ->
-                    gameService.initCarList("이름이6자리"));
+            GameService gameService = new GameService(() -> false);
+            assertThatThrownBy(() -> gameService.initCarList("pobi,woni,pobi"))
+                    .isInstanceOf(IllegalArgumentException.class);
         });
     }
 
     @Test
-    void 이름_입력없음() {
+    void 공백_포함_중복() {
         assertSimpleTest(() -> {
-            GameService gameService = new GameService(() -> true);
-            assertThrows(IllegalArgumentException.class, () ->
-                    gameService.initCarList(""));
+            GameService gameService = new GameService(() -> false);
+            assertThatThrownBy(() -> gameService.initCarList("pobi, pobi"))
+                    .isInstanceOf(IllegalArgumentException.class);
         });
     }
+
+    @Test
+    void 시도횟수_숫자() {
+        GameService gameService = new GameService(() -> false);
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> gameService.parseRounds("rockernun"))
+                    .isInstanceOf(IllegalArgumentException.class);
+            assertThatThrownBy(() -> gameService.parseRounds("a1b2c3"))
+                    .isInstanceOf(IllegalArgumentException.class);
+            assertThatThrownBy(() -> gameService.parseRounds(" "))
+                    .isInstanceOf(IllegalArgumentException.class);
+        });
+    }
+
+    @Test
+    void 시도횟수_양수() {
+        GameService gameService = new GameService(() -> false);
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> gameService.parseRounds("0"))
+                    .isInstanceOf(IllegalArgumentException.class);
+            assertThatThrownBy(() -> gameService.parseRounds("-1"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        });
+    }
+
 }
